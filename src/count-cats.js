2a17b2edb4c497d362e03d0ef800207f55a49fc3
Nikolay <nikolay.kovzik@gmail.com>
2022-04-12 16:51:06 +0300

    feat: count-cats & dream-team is solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.reduce((res, cats) => {
+    return res + cats.reduce((count, cat) => {
+      return count + (cat === '^^' ? 1 : 0)
+    }, 0)
+  }, 0)
 }
 
 module.exports = {
